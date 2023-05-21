2bc9c09ada1cccb5c56d24d26464df5ba0a4a2e1
Carms Ng <contact@carms-ng.com>
2023-05-21 11:49:03 -0400

    feat: add animated logo to hero

---
+++ src/components/AnimatedLogo.vue
@@ -0,0 +1,88 @@
+<template>
+  <p id="p-first">personal/</p>
+  <p id="p-second">universal</p>
+</template>
+
+<style scoped>
+p {
+  position: absolute;
+  font-size: 1.5rem;
+  font-weight: 600;
+  rotate: -30deg;
+
+  padding: 0.25rem 0.75rem;
+  border-radius: var(--br);
+  color: var(--off-brown);
+}
+#p-first {
+  top: 3rem;
+  left: 1rem;
+  background: var(--purple);
+  animation: bounce-in forwards 1s;
+}
+#p-second  {
+  top: 4rem;
+  left: 6rem;
+  background: var(--brown);
+  animation: bounce-in-second forwards 2s;
+}
+
+@media (min-width: 768px) {
+  p {
+    scale: 1.5;
+  }
+  #p-first {
+    top: 4rem;
+    left: 3rem
+  }
+  #p-second  {
+    top: 5rem;
+    left:12rem;
+  }
+}
+
+@media (min-width: 1024px) {
+  p {
+    scale: 2;
+  }
+  #p-first {
+    top: 5rem;
+    left: 10rem;
+  }
+  #p-second  {
+    top: 7rem;
+    left: 20rem;
+  }
+}
+
+@keyframes bounce-in {
+  0% {
+    opacity: 0;
+    transform: translateY(-2000px);
+  }
+  60% {
+    opacity: 1;
+    transform:scaleY(0.6) translateY(30px)
+  }
+  100% {
+    transform:  scaleY(1) translateY(0)
+  }
+}
+
+@keyframes bounce-in-second {
+  0% {
+    opacity: 0;
+  }
+  50% {
+    transform: translateY(-2000px);
+  }
+  80% {
+    opacity: 1;
+    transform:scaleY(0.6) translateY(30px)
+  }
+  100% {
+    transform:  scaleY(1) translateY(0)
+  }
+}
+
+</style>
--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,8 +1,11 @@
 <template>
   <section>
-    <img :src="getImageUrl(content.img.filename)" :alt=content.img.alt />
+    <div className="img-wrapper">
+      <img :src="getImageUrl(content.img.filename)" :alt=content.img.alt />
+      <AnimatedLogo />
+    </div>
     <div className="card-hero">
-      <small>{{ content.pre }}</small>
+      <small>{{ content.small }}</small>
       <h1>{{ content.h1 }}</h1>
       <p v-for="pp in content.p" :key="pp">
         {{ pp }}
@@ -20,7 +23,7 @@
 
 <style scoped>
 h1 {
-  font-size: 2.25rem;
+  font-size: 2rem;
   line-height: 1;
   margin: 0;
   color: var(--purple);
@@ -30,18 +33,20 @@ small {
   font-weight: 700;
   color: var(--purple);
 }
-img {
+.img-wrapper {
+  overflow: hidden;
+}
+.img-wrapper img {
   width: 100%;
-  object-fit: cover;
   height: 60vh;
+  object-fit: cover;
 }
 
 .card-hero {
   padding: 1rem;
   text-align: left;
   display: grid;
-  place-content: space-evenly;
-  gap: 0.5rem;
+  place-content: space-between;
   height: 40vh;
 }
 
@@ -52,7 +57,7 @@ img {
   color: #fff;
   background-color: var(--brown);
   text-align: center;
-  border-radius: 1rem;
+  border-radius: var(--br);
   display: grid;
   place-content: center;
 }
@@ -60,16 +65,15 @@ img {
   opacity: 0.95;
 }
 
-
 @media (min-width: 768px) {
   small {
-    font-size: 1.5rem;
+    font-size: 1.25rem;
   }
   h1 {
     font-size: 3rem;
   }
   p {
-    font-size: 1.5rem;
+    font-size: 1.25rem;
   }
   .btn-primary {
     font-size: 1.5rem;
@@ -85,20 +89,18 @@ img {
     display: grid;
     height: 100vh;
   }
-  section > img {
+  section > .img-wrapper {
     grid-column: 1 / 12;
     grid-row: 1 / 12;
   }
-  section > div {
+  section > .card-hero {
     grid-column-end: 11;
     grid-row-end: 11;
   }
-  img {
+
+  .img-wrapper > img {
     height: 100vh;
     object-position: 20% 80%;
-  }
-  h1 {
-    font-size: 3.5rem;
   }
 
   .card-hero {
@@ -111,11 +113,11 @@ img {
   }
 }
 
-
 </style>
 
 <script>
 import content from '../assets/content.json'
+import AnimatedLogo from './AnimatedLogo.vue'
 
 export default {
   data() {
@@ -127,6 +129,9 @@ export default {
     getImageUrl(filename) {
       return new URL(`../assets/${filename}`, import.meta.url).href
     }
+  },
+  components: {
+    AnimatedLogo,
   }
 }
 </script>
